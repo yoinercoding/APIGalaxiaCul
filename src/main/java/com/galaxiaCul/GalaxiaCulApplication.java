e0244adc21f764c58b44d3d712c72b10a6f7636a
ybello <yoinercoding@gmail.com>
2022-08-02 16:09:35 -0300

    update

--- src/main/java/com/galaxiaCul/GalaxiaCulApplication.java
+++ src/main/java/com/galaxiaCul/GalaxiaCulApplication.java
@@ -1,6 +1,5 @@
 package com.galaxiaCul;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
--- src/test/java/com/galaxiaCul/GalaxiaCulApplicationTests.java
+++ src/test/java/com/galaxiaCul/GalaxiaCulApplicationTests.java
@@ -1,4 +1,4 @@
-package com.galaxiaCul.galaxiaCul;
+package com.galaxiaCul;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
