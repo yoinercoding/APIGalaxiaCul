175713c771d424ebe31af47004fa399076d47edc
ybello <yoinercoding@gmail.com>
2022-08-03 14:46:20 -0300

    update refactor

--- src/main/java/com/galaxiaCul/GalaxiaCulApplication.java
+++ src/main/java/com/galaxiaCul/GalaxiaCulApplication.java
@@ -8,12 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import com.galaxiaCul.repository.*;
 import com.galaxiaCul.model.*;
-import com.galaxiaCul.service.GalaxiaCulServiceI;
+import com.galaxiaCul.service.GalaxiaCulService;
 
 @SpringBootApplication
 public class GalaxiaCulApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(GalaxiaCulApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(GalaxiaCulApplication.class, args);
 	}
@@ -28,10 +29,10 @@ public class GalaxiaCulApplication {
 			Planeta betasoide = new Planeta("Betasoide", 2000, 3, sentidoHorario);
 			Planeta vulcano = new Planeta("Vulcano", 1000, 5, sentidoAntihorario);
 
-			GalaxiaCulServiceI galaxiaCulService = new GalaxiaCulServiceI();
+			GalaxiaCulService galaxiaService = new GalaxiaCulService();
 			galaxiaCulService.init(ferengi, betasoide, vulcano, pronosticoRepository);
 
 			logger.info("Los datos fueros cargados correctamente");
-
+		};
 	}
 }
